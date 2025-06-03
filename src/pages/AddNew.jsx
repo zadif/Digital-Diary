aa0d3df835bea4c7fbebaf57fb61a2734dda34da
Zadif <zadifmustafa93@gmail.com>
2025-06-03 11:47:22 +0500

    Added textArea in addNew

--- src/pages/AddNew.jsx
+++ src/pages/AddNew.jsx
@@ -1,4 +1,34 @@
-function addNew() {
-  return <h1>Hidsfdsfjdsndfskjndfskjdf</h1>;
+function AddNew() {
+  return (
+    <>
+      <h1>Add New Entry</h1>
+      <div id="wrapper">
+        <form id="paper" method="get" action="">
+          <div id="margin">
+            Title: <input id="title" type="text" name="title" />
+          </div>
+          <textarea
+            placeholder="Enter something funny."
+            id="text"
+            name="text"
+            rows="4"
+            style={{
+              overflow: "hidden",
+              wordWrap: "break-word",
+              resize: "none",
+              height: "160px",
+            }}
+          ></textarea>
+          <br />
+          <input id="button" type="submit" value="Create" />
+          <button id="startBtn">Start</button>
+          <button id="stopBtn">Stop</button>
+
+          <button id="clearBtn">Clear</button>
+          <button id="copyBtn">Copy</button>
+        </form>
+      </div>
+    </>
+  );
 }
-export default addNew;
+export default AddNew;
